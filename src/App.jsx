125ec5d859995941bdeb63a5c76cdd5ed4fe546c
Amr3011 <amroosamafakharany@gmail.com>
2024-09-30 21:19:03 +0300

    Make VerifyEmail form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Register from './Pages/Register';
+//import Register from './Pages/Register';
 import ThemeToggler from './components/inputs/ThemeToggler';
+import VerifyEmail from './Pages/VerifyEmail';
 //import Login from './Pages/Login';
 
 
 export default function App() {
   return <main>
     
-    
-    <Register />
+    <VerifyEmail/>
     <ThemeToggler />
 
   </main>;
