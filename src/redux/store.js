6d3aba8e0ebc93f42af2eff0517fd3ea2501a517
Alaaeldin Hassan <3laahassan106@gmail.com>
2024-10-13 23:00:54 +0300

    Fix Redux Store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,30 +5,12 @@ import userReducer from "./reducers/userReducer"; // Already added
 // Combine all the reducers
 const rootReducer = combineReducers({
   theme: themeReducer,
-<<<<<<< HEAD
-});
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-=======
-  user: userReducer // The reducer for user registration and authentication
+  user: userReducer, // The reducer for user registration and authentication
 });
 
 // Configure the Redux store
 const store = configureStore({
-  reducer: rootReducer
->>>>>>> 3124a79b1bdb2190b4156d20a5bbb2b71075f82b
+  reducer: rootReducer,
 });
 
 export default store;
