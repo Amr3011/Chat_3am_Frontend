6e5d5ea607cf08799c3fea6510d520aaf4863c88
Alaaeldin Hassan <3laahassan106@gmail.com>
2024-10-19 05:16:13 +0300

    Fetch Private and Group chats

--- src/components/common/RightSide.jsx
+++ src/components/common/RightSide.jsx
@@ -1,11 +1,11 @@
 import chat from "../../assets/chat.svg";
-import AmigosChatting from "../../assets/Amigos Chatting.png";
+// import AmigosChatting from "../../assets/Amigos Chatting.png";
 
 export default function RightSide() {
   return (
     <div className="bg-primary text-center light:text-base-100 flex flex-col justify-center items-center p-12 w-full md:w-[60%] lg:w-[70%] s:hidden m:hidden">
       <div className="flex justify-center">
-          <img src={AmigosChatting} alt="Illustration" className="mb-8" />
+        <img src={chat} alt="Illustration" className="mb-8" />
       </div>
       <p className="text-white mx-auto p-4">
         Start Chat with your Friends, Make calls, Share your Screen and get
