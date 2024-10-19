e47ffa9abcd0aeb6ff15f07db97d08b6fd48650b
Amr3011 <amroosamafakharany@gmail.com>
2024-10-19 05:23:45 +0300

    "Updated RightSide component and integrated it into Notification and Setting pages, removed AmigosChatting image and added chat image"

--- src/components/common/RightSide.jsx
+++ src/components/common/RightSide.jsx
@@ -1,16 +1,24 @@
 import chat from "../../assets/chat.svg";
-import AmigosChatting from "../../assets/Amigos Chatting.png";
 
 export default function RightSide() {
   return (
-    <div className="bg-primary text-center light:text-base-100 flex flex-col justify-center items-center p-12 w-full md:w-[60%] lg:w-[70%] s:hidden m:hidden">
-      <div className="flex justify-center">
-          <img src={AmigosChatting} alt="Illustration" className="mb-8" />
+    <div className="min-h-screen h-screen flex flex-col bg-base-100">
+      <div className="flex w-full h-full bg-primary p-8 lg:p-12 text-white justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-full">
+          <img
+            src={chat}
+            alt="Welcome Image"
+            className="w-2/3 lg:w-3/4 mb-4 lg:mb-8"
+          />
+          <h1 className="text-2xl lg:text-4xl font-bold mb-4 lg:mb-6 text-center">
+            Welcome To Chat Community
+          </h1>
+          <p className="text-sm lg:text-lg text-center">
+            This website to cover all people against world to talk with each
+            others
+          </p>
+        </div>
       </div>
-      <p className="text-white mx-auto p-4">
-        Start Chat with your Friends, Make calls, Share your Screen and get
-        Faster Now..
-      </p>
     </div>
   );
 }
--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -2,14 +2,15 @@ import ThemeToggler from "../components/inputs/ThemeToggler";
 import LastSeen from "../components/chat/LastSeen";
 import DeleteAccount from "../components/common/DeleteAccount";
 import Logout from "../components/common/Logout";
-import RightSide from "../components/common/RightSide";
+import chat from "../assets/chat.svg";
 import { useSelector } from "react-redux";
 
 const Setting = () => {
   const userInfo = useSelector((state) => state.user.userInfo);
+
   return (
-    <div className="flex w-full">
-      <div className="w-full md:w-[40%] lg:w-[30%] p-8 bg-base-100 shadow-lg">
+    <div className="flex flex-col lg:flex-row w-full min-h-screen bg-base-100">
+      <div className="w-full lg:w-[30%] p-8 bg-base-100 shadow-lg min-h-full">
         <h1 className="text-3xl font-bold mb-8">Setting</h1>
         <div className="flex items-center mb-8">
           <img
@@ -17,14 +18,27 @@ const Setting = () => {
             alt="User Profile"
             className="w-20 h-20 rounded-full mr-6"
           />
-          <span className="text-xl font-semibold">Lavern Laboy</span>
+          <span className="text-xl font-semibold">{userInfo.name || "Lavern Laboy"}</span>
         </div>
         <ThemeToggler />
         <LastSeen />
         <DeleteAccount />
         <Logout />
       </div>
-      <RightSide />
+
+      <div className="hidden lg:flex flex-1 bg-primary justify-center items-center p-8 min-h-full">
+        <div className="text-center">
+          <img
+            src={chat} 
+            alt="Chat Illustration"
+            className="w-100 h-100 mx-auto mt-8" 
+          />
+          <h2 className="text-4xl font-bold text-white mt-6">Welcome To Chat Community</h2>
+          <p className="text-xl text-white mt-4">
+            This website is to cover all people against world to talk with each others
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
