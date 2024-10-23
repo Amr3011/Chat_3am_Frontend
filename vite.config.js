53a03c609ffc87ae6b10850111337a46b5b48aec
Mostafa Abdeal-Nasser Saied Khalil <ace13520@gmail.com>
2024-10-23 13:09:47 +0300

    Update proxy target in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "nodejs-chat-io.vercel.app"
+        target: import.meta.env.VITE_API_URL
       }
     }
   },
